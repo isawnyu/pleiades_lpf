adeb72ebe92dd0ad531e849ef869a3ff27569037
Tom Elliott <ipse@paregorios.org>
2025-10-22 06:15:05 -0400

    identifiers

--- src/pleiades_lpf/identifiers.py
+++ src/pleiades_lpf/identifiers.py
@@ -6,5 +6,64 @@
 #
 
 """
-Python 3 package template (changeme)
+Define identification classes
 """
+from validators import url as validate_url
+
+VALID_IDENTIFIER_TYPES = {"url", "alphanumeric"}
+
+
+class Identifier:
+    """
+    An identifier.
+    """
+
+    def __init__(self, id_type: str, value: str):
+        self._type = ""
+        self._value = ""
+
+    @property
+    def id_type(self) -> str:
+        """Get the identifier type."""
+        return self._type
+
+    @id_type.setter
+    def id_type(self, id_type: str):
+        """Set the identifier type."""
+        id_type = id_type.strip()
+        if id_type not in VALID_IDENTIFIER_TYPES:
+            raise ValueError(
+                f"Invalid identifier type: '{id_type}'. Expected one of {VALID_IDENTIFIER_TYPES}."
+            )
+        self._type = id_type
+
+    @property
+    def id_value(self) -> str:
+        """Get the identifier value."""
+        return self._value
+
+    @id_value.setter
+    def id_value(self, value: str):
+        """Set the identifier value."""
+        if not isinstance(value, str):
+            raise TypeError(f"Identifier value must be a string. Got '{type(value)}'")
+        value = value.strip()
+        if self._type == "alphanumeric" and not value.isalnum():
+            raise ValueError(
+                f"Alphanumeric identifier value '{value}' must contain only letters and numbers."
+            )
+        elif self._type == "url" and not validate_url(value):
+            raise ValueError(f"URL identifier value '{value}' must be a valid URL.")
+        self._value = value
+
+    def __str__(self) -> str:
+        return self.id_value
+
+
+class URLIdentifier(Identifier):
+    """
+    An identifier that is a URL.
+    """
+
+    def __init__(self, url: str):
+        Identifier.__init__(self, id_type="url", value=url)
